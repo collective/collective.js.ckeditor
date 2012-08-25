5c09bfce11091691772b4032b01a189465bb571a
JeanMichel FRANCOIS <toutpt@gmail.com>
2012-08-25 12:25:12 +0200

    start migration of samples to Plone

--- collective/js/ckeditor/sample/sample_view.py
+++ collective/js/ckeditor/sample/sample_view.py
@@ -19,3 +19,36 @@ class SampleView(BrowserView):
                                                       name="plone_portal_state")
         self.portal_url = self.portal_state.portal_url()
         self.sample_url = '%s/++resources++collective.js.ckeditor/samples' % self.portal_url
+
+
+class SamplePostedData(SampleView):
+    
+    def update(self):
+        """from PHP posteddata.php file to Plone:
+        <?php
+        if ( isset( $_POST ) )
+            $postArray = &$_POST ;            // 4.1.0 or later, use $_POST
+        else
+            $postArray = &$HTTP_POST_VARS ;    // prior to 4.1.0, use HTTP_POST_VARS
+        
+        foreach ( $postArray as $sForm => $value )
+        {
+            if ( get_magic_quotes_gpc() )
+                $postedValue = htmlspecialchars( stripslashes( $value ) ) ;
+            else
+                $postedValue = htmlspecialchars( $value ) ;
+        
+        ?>
+                <tr>
+                    <th style="vertical-align: top"><?php echo $sForm?></th>
+                    <td><pre class="samples"><?php echo $postedValue?></pre></td>
+                </tr>
+            <?php
+        }
+        ?>
+        """
+        super(SamplePostedData, self).update()
+        form = self.request.form
+        self.values = []  # {'postedValue':  value, 'sForm': key}
+        for key in form:
+            self.values.append({'sForm': key, 'postedValue': form[key]})
